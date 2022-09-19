ecab44098db006d8dfd3af90179522dce33ff7eb
Abdul Hadi <abdul.806@yahoo.com>
2022-09-19 21:25:07 +0500

    Completed counter app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+// usestate
+const [state, setState] = useState(0)
+  const handleAdd = () => {
+    if(state < 10){
+      setState(state + 1)
+    }
+  }
+  const Substact = () => {
+    if(state > 0){
+      setState(state - 1)
+    }
+  }
+  const Retry = () => {
+    setState(state - state)
+  }
+  return (
+    <div className="App">
+      <div className='background'>
+      <header className="App-header">
+      <h1>Counter</h1>
+      <span>{state}</span>
+        <div className ='button'>
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={Retry}>Reset</button>
+        <button onClick={Substact}>Substact</button>   
+        </div>
+      </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
